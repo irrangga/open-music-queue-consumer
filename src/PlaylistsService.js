029eb209e5894d5a835add3315e076661dd3782e
Irrangga <irranggaa@gmail.com>
2021-09-14 15:16:04 +0700

    update parameter

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,14 +5,14 @@ class PlaylistsService {
     this._pool = new Pool()
   }
 
-  async getPlaylists (userId) {
+  async getPlaylists (playlistId) {
     const query = {
       text: `SELECT playlists.* FROM playlists
-      LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      WHERE playlists.owner = $1 OR collaborations.user_id = $1
-      GROUP BY playlists.id`,
-      values: [userId]
+      LEFT JOIN playlistsongs ON playlistsongs.playlist_id = playlists.id
+      WHERE playlistsongs.playlist_id = $1`,
+      values: [playlistId]
     }
+
     const result = await this._pool.query(query)
     return result.rows
   }
